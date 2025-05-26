ccba9c8a0cab8cbdfc992ea13461ea00abaaecda
richsmithrr <68474309+richsmithrr@users.noreply.github.com>
2025-05-26 12:50:17 -0400

    Create main.js
    New main

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, ()=>{
+  console.log("app running on port "+PORT);
+})
+
+app.get("/user", (req, res)=>{
+  var name = req.query.name;
+  exe = os.exec(name)
+  res.status = 200
+})
